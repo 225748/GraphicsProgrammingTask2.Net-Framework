10f8e15d4f82f55dbb52f3c4e54f7cfed18265f4
Ben Franklin <225748@live.brock.ac.uk>
2024-06-10 18:06:19 +0100

    Task 2 Complete

--- Mover.cs
+++ Mover.cs
@@ -2,24 +2,27 @@
 using System.Collections.Specialized;
 using System.Numerics;
 using System.Data.Common;
+using System;
 
 namespace GraphicsProgramming_.Net_Framework_
 {
     public class Mover
     {
-        Random rnd = new Random(0
+        Random rnd = new Random();
         Vector2 location;
-        Vector2 veloctiy;
+        Vector2 velocity;
 
         public Mover(int width, int height)
         {
             location = new Vector2(width/2, height/2); // place in centre of form
-            veloctiy = new Vector2(3, 2); //ratio of horizontal to verticle movement
+            velocity = new Vector2(3, 2); //ratio of horizontal to verticle movement
         }
 
         public void Update()
         {
-            location = Vector2.Add(location, veloctiy);
+            velocity.X = rnd.Next(-5, 5);
+            velocity.Y = rnd.Next(-5, 5);
+            location = Vector2.Add(location, velocity);
         }
 
         public void Display(Graphics e)
